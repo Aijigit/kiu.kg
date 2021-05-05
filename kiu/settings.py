dac266ab059b9b766d3e5244fb35ccf728b62624
Aijigit <ajzigit24@gmail.com>
2021-05-06 02:42:19 +0600

    Test2 KIU.KG

--- kiu/settings.py
+++ kiu/settings.py
@@ -70,6 +70,12 @@ WSGI_APPLICATION = 'kiu.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'kiu',
         'USER': 'admin',
@@ -77,8 +83,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432'
     }
-}
-
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
